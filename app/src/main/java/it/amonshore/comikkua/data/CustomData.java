d85c4121d3461eeaefcde625245928b3e6725d7d
narsenico <not_amon-dev@yahoo.it>
2020-08-17 13:44:52 +0200

    Error without message

--- app/src/main/java/it/amonshore/comikkua/data/CustomData.java
+++ app/src/main/java/it/amonshore/comikkua/data/CustomData.java
@@ -1,16 +1,27 @@
 package it.amonshore.comikkua.data;
 
 import com.android.volley.Response;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 
 import androidx.lifecycle.MutableLiveData;
+import it.amonshore.comikkua.Utility;
 
 public class CustomData<T> extends MutableLiveData<Resource<T>>
         implements Response.Listener<T>, Response.ErrorListener {
 
     @Override
     public void onErrorResponse(VolleyError error) {
-        postValue(Resource.error(null, error.getMessage()));
+        final String message = error.getMessage();
+        if (Utility.isNullOrEmpty(message)) {
+            if (error instanceof TimeoutError) {
+                postValue(Resource.error(null, "Timeout"));
+            } else {
+                postValue(Resource.error(null, String.format("Unknown error: '%s'", error.getClass().getName())));
+            }
+        } else {
+            postValue(Resource.error(null, error.getMessage()));
+        }
     }
 
     public void onErrorResponse(Exception error) {
