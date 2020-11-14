c8321067328dbdc3fb8244cf6912f135067a3789
narsenico <not_amon-dev@yahoo.it>
2020-11-14 15:36:51 +0100

    Uso searchableName

--- app/src/main/java/it/amonshore/comikkua/data/web/FirebaseRepository.java
+++ app/src/main/java/it/amonshore/comikkua/data/web/FirebaseRepository.java
@@ -1,8 +1,13 @@
 package it.amonshore.comikkua.data.web;
 
-import com.google.firebase.firestore.FieldPath;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -27,7 +32,6 @@ public class FirebaseRepository {
 
     public FirebaseRepository() {
         mFirestore = FirebaseFirestore.getInstance();
-
         mReleaseDateFormat = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
 
         LogHelper.d("CMKWEB/Firestore using projectId " + mFirestore.getApp().getOptions().getProjectId());
@@ -45,7 +49,6 @@ public class FirebaseRepository {
         liveData.postValue(Resource.loading(null));
         // estraggo tutti i documenti dalla collection "comics"
         // ogni documento è un comics, che possiede la proprietà "name"
-        // non uso direttamente l'id del comics perché è composta da nome (normalizzato) e reissue
         mFirestore.collection("comics")
                 .get()
                 .addOnCompleteListener(task -> {
@@ -77,51 +80,66 @@ public class FirebaseRepository {
         return liveData;
     }
 
+    public CustomData<List<CmkWebComics>> getComics() {
+        // TODO: da implementare
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
     /**
      * Cerca la nuova release di un comics a partire da un dato numero (compreso).
-     *
-     * TODO: considerare reissue
      *
      * @param title      titolo del comics per cui cercare la nuova release
      * @param numberFrom numero da cui iniziare la ricerca (compreso)
      * @return nuova release
+     * @deprecated Usare getReleases passando anche il numero di ristampa
      */
+    @Deprecated
     public CustomData<List<CmkWebRelease>> getReleases(String title, int numberFrom) {
-        LogHelper.d("CMKWEB/Firestore read '%s' with release >= %s", title, numberFrom);
+        return getReleases(title, 0, numberFrom);
+    }
 
+    /**
+     * Cerca la nuova release di un comics a partire da un dato numero (compreso).
+     *
+     * @param title      titolo del comics per cui cercare la nuova release
+     * @param reissue    numero di ristampa (0 per la prima edizione)
+     * @param numberFrom numero da cui iniziare la ricerca (compreso)
+     * @return nuova release
+     */
+    public CustomData<List<CmkWebRelease>> getReleases(String title, int reissue, int numberFrom) {
         final CustomData<List<CmkWebRelease>> liveData = new CustomData<>();
         liveData.postValue(Resource.loading(null));
 
-        // carico il documento relativo al comics
-        // considerando che l'id è composto da <nome normalizzato>_<reissue>
-        // inoltre eseguo un query sull'id della release, ricordando che è una stringa
+        final String searchableName = formatComicsId(title, reissue);
+
+        LogHelper.d("CMKWEB/Firestore read '%s' with release >= %s",
+                searchableName, numberFrom);
+
+        // eseguo una query sul nome
+        // in teoria dovrebbe ritornare un solo comics, ma mi tocca gestire risultati multipli
+        // per ogni comcis estraggo le release
         mFirestore.collection("comics")
-                .document(formatComicsId(title, 0))
-                .collection("releases")
-                .whereGreaterThanOrEqualTo("releaseNumber", numberFrom)
+                .whereEqualTo("searchableName", searchableName)
                 .get()
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        final QuerySnapshot result = task.getResult();
-                        if (result == null) {
-                            // nessun risultato, ritorno una lista vuota
-                            liveData.postValue(Resource.success(Collections.emptyList()));
-                        } else {
-                            final List<CmkWebRelease> releases = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : result) {
-                                releases.add(fromDocument(title, document));
-                            }
-                            liveData.postValue(Resource.success(releases));
-                        }
-                    } else {
-                        final Exception error = task.getException();
-                        LogHelper.e("Error loading releases", error);
-                        if (error == null) {
-                            liveData.postValue(Resource.error(null, "Unknown error"));
-                        } else {
-                            liveData.postValue(Resource.error(null, error.getMessage()));
-                        }
+                    List<Task<QuerySnapshot>> tasks = new ArrayList<>();
+                    for (DocumentSnapshot ds : task.getResult()) {
+                        LogHelper.d("CMKWEB/Firestore found '%s'", ds.getId());
+                        tasks.add(ds.getReference().collection("releases")
+                                .whereGreaterThanOrEqualTo("releaseNumber", numberFrom)
+                                .get());
                     }
+
+                    Tasks.whenAllSuccess(tasks).addOnCompleteListener(task1 -> {
+                        final List<CmkWebRelease> releases = new ArrayList<>();
+                        for (Object o : task1.getResult()) {
+                            final QuerySnapshot qs = (QuerySnapshot)o;
+                            for (QueryDocumentSnapshot qds : qs) {
+                                releases.add(fromDocument(title, qds));
+                            }
+                        }
+                        liveData.postValue(Resource.success(releases));
+                    });
                 });
 
         return liveData;
@@ -133,7 +151,8 @@ public class FirebaseRepository {
                 reissue);
     }
 
-    private CmkWebRelease fromDocument(@NonNull String title, @Nonnull QueryDocumentSnapshot document) {
+    private CmkWebRelease fromDocument(@NonNull String title, @Nonnull QueryDocumentSnapshot
+            document) {
         final CmkWebRelease release = new CmkWebRelease();
         release.title = title;
         release.number = Integer.parseInt(document.getId());
