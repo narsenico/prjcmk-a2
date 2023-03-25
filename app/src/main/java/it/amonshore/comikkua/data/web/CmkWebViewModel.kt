825d975cf388e6458ac8d1ae1c2c00a642a31b19
narsenico <not_amon-dev@yahoo.it>
2023-03-25 14:15:00 +0100

    Rimosso codice inutile

--- app/src/main/java/it/amonshore/comikkua/data/web/CmkWebViewModel.kt
+++ app/src/main/java/it/amonshore/comikkua/data/web/CmkWebViewModel.kt
@@ -20,10 +20,15 @@ class CmkWebViewModel(application: Application) : AndroidViewModel(application) 
     private var _lastFilter: String = ""
     private var _filteringJob: Job? = null
 
+    var filter: String
+        get() = _lastFilter
+        set(value) {
+            _lastFilter = value
+            _filter.postValue(_lastFilter)
+        }
+
     @FlowPreview
     private val _filterFlow: Flow<List<String>> = flow {
-        // ogni volta che viene modificato il filtro emetto una lista con tutte le parole contenute nel filtro
-        // se il filtro è vuoto emetto null
         _filter.asFlow()
             .debounce(FILTER_DEBOUNCE)
             .map { it.trim() }
@@ -38,50 +43,13 @@ class CmkWebViewModel(application: Application) : AndroidViewModel(application) 
             .collect { emit(it) }
     }
 
-    /**
-     * Filtro applicato ai comics letti con [getFilteredAvailableComics]
-     * su [AvailableComics.name] e [AvailableComics.publisher]
-     */
-    var filter: String
-        get() = _lastFilter
-        set(value) {
-            _lastFilter = value
-            _filter.postValue(_lastFilter)
-        }
-
-    fun useLastFilter(): String {
-        _filter.postValue(_lastFilter)
-        return _lastFilter
-    }
-
-//    /**
-//     * Ritorna un [PagingData] con i comics disponibii,
-//     * eventualmente filtrati grazie a [CmkWebViewModelKt.filter]
-//     */
-//    fun getAvailableComics(): LiveData<PagingData<AvailableComics>> {
-//        val currentData = _filteredAvailableComics
-//        return if (currentData != null) {
-//            currentData
-//        } else {
-//
-//            // TODO: An instance of PagingSource was re-used when Pager expected to create a new
-//
-//            val data = Transformations.switchMap(_filter) {
-//                // se il filtro non è nullo sostituisco gli spazi vuoti con "%" e racchiudo tutto tra "%"
-//                val likeName = it?.let {
-//                    "%${it.replace("\\s+".toRegex(), "%")}%"
-//                }
-//                _repository.getAvailableComicsPagingSourceLiveData(likeName)
-//                        .cachedIn(viewModelScope)
-//            }
-//            _filteredAvailableComics = data
-//            data
-//        }
+//    fun useLastFilter(): String {
+//        _filter.postValue(_lastFilter)
+//        return _lastFilter
 //    }
 
     /**
      * LiveData con i comics disponibili filtrati grazie alla proprietà [CmkWebViewModel.filter].
-     * Se il filtro è vuoto viene emessa una lista vuota.
      */
     @OptIn(FlowPreview::class)
     fun getFilteredAvailableComics(): LiveData<List<AvailableComics>> = liveData {
