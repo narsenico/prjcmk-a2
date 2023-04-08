fdedcab1e1984c8369a9c5aa147c87f8e08be1e0
narsenico <not_amon-dev@yahoo.it>
2023-04-08 20:29:16 +0200

    Convertito IViewHolderWithDetails in kotlin

--- app/src/main/java/it/amonshore/comikkua/ui/IViewHolderWithDetails.kt
+++ app/src/main/java/it/amonshore/comikkua/ui/IViewHolderWithDetails.kt
@@ -1,16 +1,11 @@
-package it.amonshore.comikkua.ui;
+package it.amonshore.comikkua.ui
 
-import android.view.View;
+import android.view.View
+import androidx.recyclerview.selection.ItemDetailsLookup.ItemDetails
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.recyclerview.selection.ItemDetailsLookup;
-import androidx.recyclerview.widget.RecyclerView;
-
-public abstract class IViewHolderWithDetails<K> extends RecyclerView.ViewHolder {
-
-    public IViewHolderWithDetails(View itemView) {
-        super(itemView);
-    }
-
-    public abstract ItemDetailsLookup.ItemDetails<K> getItemDetails();
-
+abstract class IViewHolderWithDetails<K>(itemView: View?) : RecyclerView.ViewHolder(
+    itemView!!
+) {
+    abstract val itemDetails: ItemDetails<K>?
 }
