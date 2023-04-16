af3922232bf061d0e939e4a16b0dfc824e535ba6
narsenico <not_amon-dev@yahoo.it>
2023-04-16 17:33:43 +0200

    Refactoring in kotlin

---
+++ app/src/main/java/it/amonshore/comikkua/data/release/ReleaseHeader.kt
@@ -0,0 +1,24 @@
+package it.amonshore.comikkua.data.release
+
+import it.amonshore.comikkua.Constants
+
+data class ReleaseHeader(
+    private val relativeId: Long,
+    @Constants.ReleaseTypeDef val type: Int,
+    var totalCount: Int = 0,
+    var purchasedCount: Int = 0
+) : IReleaseViewModelItem {
+
+    private val _id = BASE_ID + relativeId
+
+    override val id: Long
+        get() = _id
+
+    override val itemType: Int
+        get() = ITEM_TYPE
+
+    companion object {
+        const val ITEM_TYPE = 1
+        const val BASE_ID = 90000000L
+    }
+}
