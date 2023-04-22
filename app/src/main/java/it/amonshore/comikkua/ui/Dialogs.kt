201216fc6f3aff4210c14a23814d71d42449fd26
narsenico <not_amon-dev@yahoo.it>
2023-04-22 16:57:58 +0200

    Aggiunto dialog helper

---
+++ app/src/main/java/it/amonshore/comikkua/ui/Dialogs.kt
@@ -0,0 +1,62 @@
+package it.amonshore.comikkua.ui
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import androidx.core.widget.doAfterTextChanged
+import it.amonshore.comikkua.databinding.DialogConfirmBinding
+
+fun showCancellableDialog(
+    activity: Activity,
+    title: String,
+    message: String,
+    onCancel: () -> Unit
+): Dialog = AlertDialog.Builder(activity)
+    .setTitle(title)
+    .setMessage(message)
+    .setCancelable(false)
+    .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+        dialog.dismiss()
+        onCancel()
+    }
+    .create()
+    .also {
+        it.show()
+    }
+
+fun showConfirmDialog(
+    activity: Activity,
+    title: String,
+    message: String,
+    confirmPhrase: String,
+    onAccept: () -> Unit,
+): Dialog {
+    val inflater = activity.layoutInflater;
+    val binding = DialogConfirmBinding.inflate(inflater)
+    binding.message.text = message
+
+    val dialog = AlertDialog.Builder(activity)
+        .setTitle(title)
+        .setView(binding.root)
+        .setCancelable(false)
+        .setPositiveButton(android.R.string.ok) { _, _ ->
+            onAccept()
+        }
+        .setNegativeButton(android.R.string.cancel, null)
+        .create()
+
+    binding.txtConfirm.doAfterTextChanged { text ->
+        text?.run {
+            dialog.getButton(DialogInterface.BUTTON_POSITIVE).isEnabled =
+                toString() == confirmPhrase
+        }
+    }
+
+    dialog.setOnShowListener {
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE)?.isEnabled = false
+    }
+
+    dialog.show()
+    return dialog
+}
