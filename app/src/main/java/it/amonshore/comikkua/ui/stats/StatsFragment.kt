7397b17220232e46b97c050cb73a650a4cd720df
narsenico <not_amon-dev@yahoo.it>
2023-04-25 00:41:43 +0200

    Provato uistate in compose

--- app/src/main/java/it/amonshore/comikkua/ui/stats/StatsFragment.kt
+++ app/src/main/java/it/amonshore/comikkua/ui/stats/StatsFragment.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package it.amonshore.comikkua.ui.stats
 
 import android.os.Bundle
@@ -9,11 +11,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
@@ -22,8 +34,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 import it.amonshore.comikkua.R
 import it.amonshore.comikkua.toHumanReadable
@@ -41,13 +55,14 @@ class StatsFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                val comicsCount by viewModel.comicsCount.observeAsState(0)
-                val lastUpdate by viewModel.lastUpdate.observeAsState(null)
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
                 Mdc3Theme {
                     StatsScreen(
-                        comicsCount = comicsCount,
-                        lastUpdate = lastUpdate
+                        comicsCount = uiState.comicsCount,
+                        lastUpdate = uiState.lastUpdate,
+                        isLoading = uiState.isLoading,
+                        onDeleteAllCLick = viewModel::deleteAll
                     )
                 }
             }
@@ -58,11 +73,19 @@ class StatsFragment : Fragment() {
 @Composable
 fun StatsScreen(
     comicsCount: Int,
-    lastUpdate: ZonedDateTime?
+    lastUpdate: ZonedDateTime?,
+    isLoading: Boolean = false,
+    onDeleteAllCLick: () -> Unit,
 ) {
     val context = LocalContext.current
+    var showConfirmDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     Column(Modifier.fillMaxSize()) {
+        if (isLoading) {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+        }
         StatsCounterRow(
             label = stringResource(id = R.string.stats_comics_count),
             value = comicsCount.toString()
@@ -73,6 +96,25 @@ fun StatsScreen(
             value = lastUpdate?.toHumanReadable(context) ?: ""
         )
         Divider()
+        Button(
+            onClick = { showConfirmDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(stringResource(id = R.string.delete_all))
+        }
+    }
+
+    if (showConfirmDialog) {
+        ConfirmDialog(
+            title = stringResource(id = R.string.confirm_title),
+            message = stringResource(
+                id = R.string.confirm_delete_comics_with_confirm_phrase,
+                "DELETE"
+            ),
+            confirmPhrase = "DELETE",
+            onConfirm = onDeleteAllCLick,
+            onDismiss = { showConfirmDialog = false }
+        )
     }
 }
 
@@ -98,12 +140,80 @@ fun StatsCounterRow(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ConfirmDialog(
+    title: String,
+    message: String,
+    confirmPhrase: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    var phrase by rememberSaveable {
+        mutableStateOf("")
+    }
+    var buttonEnabled by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    // https://m3.material.io/components/dialogs/specs
+    Dialog(onDismissRequest = onDismiss) {
+        Card {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            bottom = 16.dp
+                        ),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = message,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                TextField(
+                    value = phrase,
+                    onValueChange = {
+                        phrase = it
+                        buttonEnabled = phrase == confirmPhrase
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                TextButton(
+                    enabled = buttonEnabled,
+                    onClick = {
+                        onDismiss()
+                        onConfirm()
+                    },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(4.dp)
+                ) {
+                    Text(stringResource(id = android.R.string.ok))
+                }
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun StatsScreenPreview() {
     Mdc3Theme {
-        StatsScreen(comicsCount = 100, lastUpdate = ZonedDateTime.now())
+        StatsScreen(
+            comicsCount = 100,
+            lastUpdate = ZonedDateTime.now(),
+            onDeleteAllCLick = { }
+        )
     }
 }
 
@@ -112,5 +222,19 @@ fun StatsScreenPreview() {
 fun StatsCounterRowPreview() {
     Mdc3Theme {
         StatsCounterRow(label = "Counter", value = "100")
+    }
+}
+
+@Preview
+@Composable
+fun ConfirmDialogPreview() {
+    Mdc3Theme {
+        ConfirmDialog(
+            title = "Confirm",
+            message = "Enter CONFIRM to confirm",
+            confirmPhrase = "CONFIRM",
+            onConfirm = {},
+            onDismiss = {}
+        )
     }
 }
--- app/src/main/java/it/amonshore/comikkua/ui/stats/StatsViewModel.kt
+++ app/src/main/java/it/amonshore/comikkua/ui/stats/StatsViewModel.kt
@@ -2,22 +2,52 @@ package it.amonshore.comikkua.ui.stats
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import it.amonshore.comikkua.data.comics.ComicsRepository
 import it.amonshore.comikkua.data.release.ReleaseRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
+
+data class StatsUiState(
+    val comicsCount: Int = 0,
+    val lastUpdate: ZonedDateTime? = null,
+    val isLoading: Boolean = false
+)
 
 class StatsViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _comicsRepository = ComicsRepository(application)
     private val _releaseRepository = ReleaseRepository(application)
 
-    val comicsCount: LiveData<Int> = liveData {
-        emit(_comicsRepository.count())
+    private val _uiState = MutableStateFlow(StatsUiState(isLoading = true))
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            refreshState()
+        }
     }
 
-    val lastUpdate: LiveData<ZonedDateTime?> = liveData {
-        emit(_releaseRepository.getLastUpdate())
+    private suspend fun refreshState() {
+        val state = StatsUiState(
+            comicsCount = _comicsRepository.count(),
+            lastUpdate = _releaseRepository.getLastUpdate(),
+            isLoading = false
+        )
+        _uiState.value = state
+    }
+
+    private fun loading() = _uiState.update { state -> state.copy(isLoading = true) }
+
+    fun deleteAll() = viewModelScope.launch {
+        loading()
+        // TODO
+//        _comicsRepository.deleteAll(deleteImages = true)
+        delay(2000)
+        refreshState()
     }
 }
